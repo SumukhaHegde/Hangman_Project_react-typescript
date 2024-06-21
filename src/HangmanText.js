82ef0dc7856d60cba4fa579a0da2385ecae820a3
Sumukha <sumukahegde007@gmail.com>
2024-06-21 23:24:59 +0530

    hangman text

--- src/HangmanText.js
+++ src/HangmanText.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const HangmanText = () => {
-  return <div>text</div>;
+  const text = ["t", "e", "x", "t"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "1rem",
+        alignContent: "center",
+        justifyContent: "center",
+      }}
+    >
+      {text.map((letter, index) => (
+        <div
+          key={index}
+          style={{
+            borderBottom: "10px solid black",
+            width: "50px",
+            textAlign: "center",
+          }}
+        >
+          {letter}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default HangmanText;
