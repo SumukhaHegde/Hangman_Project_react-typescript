45785bba58fb985fd3f14ad8b1583c05dab5798e
Sumukha <sumukahegde007@gmail.com>
2024-06-21 23:34:03 +0530

    hangman text

--- src/HangmanKeyboard.js
+++ src/HangmanKeyboard.js
@@ -1,5 +1,34 @@
 import React from "react";
 
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
 const HangmanKeyboard = () => {
   return <div>HangmanKeyboard</div>;
 };
